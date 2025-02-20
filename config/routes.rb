c552a5961a7621daeba3c5ee89441805ac16b054
Aditya Kothari <adityakothari99@gmail.com>
2025-02-20 23:26:15 +0000

    added final answers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
 
   get("/", { :controller => "items", :action => "index" })
+
+  get("/backdoor", { :controller => "items", :action => "display_form"})
+  post("/create_item", { :controller => "items", :action => "create_new_item"})
 end
